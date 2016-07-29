03e6847e514227fec445b7eb1003571971a27bf7
meshnesh <tonny.nesh@gmail.com>
2016-07-29 12:27:09 +0300

    added the complete styles and linked all the pages

--- login/js/app.js
+++ login/js/app.js
@@ -2,8 +2,10 @@ function checkPass() {
     //Store the password field objects into variables ...
     var password = document.getElementById('password');
     var pass2 = document.getElementById('pass2');
+    var email = document.getElementById('email');
     //Store the Confimation Message Object ...
     var message = document.getElementById('confirmMessage');
+    var message2 = document.getElementById('confirmMessage2');
     //Set the colors we will be using ...
     var goodColor = "#66cc66";
     var badColor = "#ff6666";
@@ -20,7 +22,17 @@ function checkPass() {
                 , data: form.serialize()
                 , dataType: 'json'
                 , success: function (result) {
-                    console.log(result)
+                    console.log(result);
+
+                    if (result == "SUCCESS") {
+                        window.location.href = '../quote/quote.html';
+                    } else {
+                        // alert('EMAIL ALREADY EXISTS');
+                        email.style.backgroundColor = badColor;
+                        message2.style.color = badColor;
+                        message2.innerHTML = "EMAIL ALREADY EXISTS!";
+                    }
+
                 }
             });
         });
@@ -42,80 +54,40 @@ function checkPass() {
 
 
 
-$('form').on('submit', function (event) {
-    event.preventDefault();
-    var form = $(this);
-    $("#btnGetResponse").click(function () {
-        $.ajax({
-            type: "POST"
-            , url: 'http://symatechlabs.com/bima/api/userLogin.php'
-            , data: form.serialize()
-            , dataType: "json"
-            , beforeSend: function () {
-                $('#spinner').show();
-            }
-            , complete: function () {
-                $('#spinner').hide();
-            },
+        $('form').on('submit', function (event) {
+            event.preventDefault();
+            var form = $(this);
+            $("#btnGetResponse").click(function () {
+                    $.ajax({
+                    type: "POST"
+                    , url: 'http://symatechlabs.com/bima/api/userLogin.php'
+                    , data: form.serialize()
+                    , dataType: "json"
+                    , beforeSend: function () {
+                        $('.loader').show();
+                    }
+                    , complete: function () {
+                        $('.loader').hide();
+                    },
+
+                    success: function (d) {
 
 
-            success: function (d) {
-                console.log(d)
-                if (d.userLogin[0].result == "SUCCESS") {
 
-                    window.location.href = 'https://www.google.com/';
-                    // setTimeout(document.getElementById("spinner").style.display="none", 5000); 
-                } else {
-                    alert('Wrong credentials');
-                }
-            }
+                        if (d.userLogin[0].result == "SUCCESS") {
+                            console.log(d);
+                            window.location.href = '../quote/index.html';
+                        } else {
+                            // alert('Wrong credentials');
+
+
+                            document.getElementById('confirmMessage3').innerHTML = "EMAIL AND PASSWORD DO NOT MATCH!"
+                        }
+                    }
+
+                });
+
+
+            });
         });
-    });
-});
 
-
-
-
-
-
-//$('form').on('submit', function (event) {
-//    event.preventDefault();
-//    var form = $(this);
-//    $("#btnGetResponse").click(function () {
-//        $.ajax({
-//            type: "POST"
-//            , url: 'http://symatechlabs.com/bima/api/userLogin.php'
-//            , data: form.serialize()
-//            , dataType: "json"
-//            , success: function (d) {
-//                if (d.r == "fail") {
-//                    alert('wrong password and email');
-//                } else {
-//                    // alert('would you like to be redirected?');
-//                    window.location.href = 'login/user/index.html';
-//
-//                }
-//            }
-//        });
-//    });
-//});
-
-
-
-
-
-
-
-
-//$('form').on('submit', function (event) {
-//    event.preventDefault();
-//    var form = $(this);
-//    $.ajax('http://symatechlabs.com/bima/api/userLogin.php', {
-//        type: 'POST'
-//        , data: form.serialize()
-//        , dataType: 'json'
-//        , success: function (result) {
-//            console.log(result)
-//        }
-//    });
-//});
